de4cf6d7798a4be48379be7e85693ac3df90fdc7
r181104 <sten181104@gmail.com>
2025-09-01 13:08:26 +0530

    s

---
+++ main.java
@@ -0,0 +1,15 @@
+class main 
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello World! in JAVA");
+	}
+}
+
+class nothing 
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("I don't really understand what am i doing");
+	}
+}
